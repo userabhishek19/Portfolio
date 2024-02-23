9f965851cd4772cd8fe163f042c6a99cbb5f1c38
userabhishek19 <121710381+userabhishek19@users.noreply.github.com>
2024-02-23 22:44:20 +0530

    depoloyed full website

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false);
+
+    const handleNav = () => {
+        setNav(!nav);
+    }
+
+    return (
+        <div className='z-10 text-gray-500 flex justify-between items-center max-w-[1240px] mx-auto h-24 px-4 text-1'>
+     
+            <h1 className='text-3xl font-bold primary-color ml-4'>Abhishek</h1>
+
+           
+            <ul className='hidden md:flex'>
+                <li className='p-2'>Home</li>
+                <li className='p-2'>Company</li>
+                <li className='p-2'>Resources</li>
+                <li className='p-2'>About</li>
+                <li className='p-2'>Contact</li>
+            </ul>
+
+            {/* Mobile menu icon */}
+            <div onClick={handleNav} className='block md:hidden'>
+                {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
+            </div>
+
+            {/* Mobile menu */}
+            <div className={nav ? 'text-gray-300 fixed h-full left-0 top-0 w-[60%] border-r border-r-gray-900 bg-[#202121] ease-in-out duration-500'
+             : 'fixed left-[-100%]'}>
+                <h1 className='text-3xl font-bold primary-color m-4'>Abhishek</h1>
+                <ul className='p-8 text-2xl'>
+                    <li className='p-2'>Home</li>
+                    <li className='p-2'>Company</li>
+                    <li className='p-2'>Resources</li>
+                    <li className='p-2'>About</li>
+                    <li className='p-2'>Contact</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
---
+++ src/components/hero.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import profilepic from '../assets/profilepic5.png'
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin, AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import { TypeAnimation} from 'react-type-animation';
+
+
+
+
+
+
+const hero = () => {
+    return (
+      <div>
+          <div className='my-7 sm:my-0 max-w-[1200px] h-[80vh] mx-auto
+           flex flex-col-reverse sm:flex-row justify-venter align-center'>
+
+            <div className='flex-col my-auto mx-auto'>
+                <p className='md:text-5xl sm;text-4xl text-xl font-bold text-gray-200'>Hi! I am Abhishek Jha</p>
+
+                <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>
+                    <TypeAnimation
+                    sequence={[
+                        "Frontend Dev",
+                        1000,
+                        "Webdesigner",
+                        1000,
+                        "Consultant",
+                        1000,
+                    ]}
+                    wrapper='span'
+                    speed={50}
+                    repeat={Infinity}
+                    />
+                </h1>
+            <div  className='flex justify-center items-center'>
+            <p className='md:text-5xl sm;text-4xl text-xl font-bold text-gray-500'>proficient beginner, Developing Skills</p>
+
+
+            </div>
+<div className='text-5xl flex justify-start gap-16 my-7 text-purple-600'>
+    <AiFillLinkedin/>
+    <AiFillGithub/>
+    <AiFillInstagram/>
+
+</div>
+<div class="relative inline-flex group my-3">
+    <div class="absolute transition-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] 
+     to-[#FF675E] rounded-xl blur-ig group-hover:opacity-100
+      group-hover:-inset-1 group-hover:duration-200 animate-tilt">
+
+    </div>
+    <a href='/' title="Download CV" role="buttom"
+    class="w-[190px] h-[60px] relative inline-flex items-center justify-center
+     px-8 py-4 text-lg font-bold text-white transition-all duration-200
+      bg-primary-color font-pj rounded-xl
+    focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 ">DownLoad CV
+    </a>
+</div>
+            </div>
+              
+         <div className='text-8xl'>
+         
+         </div>
+         <div className='my-auto'>
+          <img className='w-[300px] sm:w-[500px] mx-auto h-auto ' src={profilepic} alt="profile pic" />
+         </div>
+  
+  
+  
+          </div>
+        
+      </div>
+    )
+  }
+  
+  export default hero
+  
